8f0181918ddbc5f16cc6d4be75314c1aa73acb5c
Christian Reisancho <geniopol@gmail.com>
2021-10-30 01:33:06 -0500

    Agregar archivos de proyecto.

---
+++ TS3Reisancho/TS3Reisancho/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TS3Reisancho
+{ 
+public partial class MainPage : ContentPage
+{
+    public static String NombreUsuario = "estudiante2021";
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void btnAbrir_Clicked(object sender, EventArgs e)
+    {
+        string usuario = txtUser.Text;
+        string clave = txtPass.Text;
+        if ((usuario == "estudiante2021") && (clave == "uisrael2021"))
+
+            await Navigation.PushAsync(new viewDos(usuario, clave));
+
+
+        else
+        {
+            lblResultado.Text = "usario o contraseña incorrectos";
+        }
+    }
+}
+
+}
---
+++ TS3Reisancho/TS3Reisancho/viewDos.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TS3Reisancho
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewDos : ContentPage
+    {
+        public viewDos(string usuario, string clave)
+        {
+            InitializeComponent();
+            lblUser.Text = usuario;
+        }
+
+        private void btnTotal_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double s1 = Convert.ToDouble(TxtDato1.Text);
+                double exs1 = Convert.ToDouble(TxtDato2.Text);
+                double s2 = Convert.ToDouble(TxtDato3.Text);
+                double ex2 = Convert.ToDouble(TxtDato4.Text);
+
+                double total = ((s1 * 0.3) + (exs1 * 0.2)) + ((s2 * 0.3) + (ex2 * 0.2));
+
+                TxtTotal.Text = total.ToString();
+                if (total >= 7)
+                {
+                    DisplayAlert("Felicitaciones", "APROBADO", "SALIR");
+                }
+                else
+                {
+                    DisplayAlert("Sigue Intentando", "REPROBADO", "SALIR");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
